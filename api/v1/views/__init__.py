5d2da7ffc251a2fdb0777cfb3ef6b02c2d34b6a5
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-10-23 11:38:54 +0000

    change state to states

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -13,7 +13,7 @@ user_views = Blueprint('users', __name__, url_prefix='/api/v1/')
 place_views = Blueprint('places', __name__, url_prefix='/api/v1/')
 review_views = Blueprint('reviews', __name__, url_prefix='/api/v1/')
 from api.v1.views.index import *
-from api.v1.views.state import *
+from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
