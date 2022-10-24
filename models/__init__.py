16de38ab4cb80993d778018d26ef86b9e2ef3e0b
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-10-24 22:37:51 +0000

    create classes getter

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,28 @@
 initialize the models package
 """
 
+
 from os import getenv
+
+
+def get_classes():
+    """ returns a dict of all classes """
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.user import User
+    from models.state import State
+    from models.review import Review
+
+    class_dict = {
+            "Amenity": Amenity,
+            "City": City,
+            "Place": Place,
+            "User": User,
+            "State": State,
+            "Review": Review
+            }
+    return class_dict
 
 
 storage_t = getenv("HBNB_TYPE_STORAGE")
